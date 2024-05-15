4680845c21a968599c8bca7cd943c469171fe041
Eric Millan <114440467+emillan-gov@users.noreply.github.com>
2024-05-15 13:47:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# src/main.py
 
+import subprocess
+import os
+
+def run_subprocess():
+    subprocess.run(["python", os.path.join(os.path.dirname(__file__), "subprocess.py")])
+
+if __name__ == "__main__":
+    print("Running main script")
+    run_subprocess()
